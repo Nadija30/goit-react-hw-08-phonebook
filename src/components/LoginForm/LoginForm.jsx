3802fa75091bedd6cc2cb7cac31b13ac930ab55a
Nadija30 <126283020+Nadija30@users.noreply.github.com>
2023-10-20 18:44:48 +0300

    commit

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -1,8 +1,75 @@
+import { toast } from 'react-toastify';
+import { logIn } from 'redux/auth/authThunk';
+const { useState } = require('react');
+const { useDispatch } = require('react-redux');
+
 const LoginForm = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const dispatch = useDispatch();
+
+  const handleChangeInput = e => {
+    const { name, value } = e.currentTarget;
+    switch (name) {
+      case 'email':
+        setEmail(value);
+        break;
+      case 'password':
+        setPassword(value);
+        break;
+
+      default:
+        break;
+    }
+  };
+
+  const hendleSubmitLogin = event => {
+    event.preventDefault();
+    const loginUser = { email, password };
+    console.log('logInUser', loginUser);
+    dispatch(logIn(loginUser))
+      .unwrap()
+      .then(originalPromiseResult => {
+        toast.success(`${originalPromiseResult.loginUser.name} welcome back!`);
+      })
+      .catch(() => {
+        toast.failure('Incorrect login or password');
+      });
+
+    setEmail('');
+    setPassword('');
+  };
+
   return (
-    <>
-      <h1>Тут буд форма для логіна</h1>
-    </>
+    <form onSubmit={hendleSubmitLogin}>
+      <label>
+        Email
+        <input
+          type="email"
+          name="email"
+          value={email}
+          pattern="/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/."
+          title="Email may contain letters, numbers, an apostrophe, and must be followed by '@' domain name '.' domain suffix. For example Taras@ukr.ua, adrian@gmail.com, JacobM3rcer@hotmail.com"
+          required
+          placeholder="Enter email ..."
+          onChange={handleChangeInput}
+        />
+      </label>
+      <label>
+        Password
+        <input
+          type="password"
+          name="password"
+          value={password}
+          pattern="^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$"
+          title="Password must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters. For example TgeV23592, 3Greioct."
+          required
+          placeholder="Enter password ..."
+          onChange={handleChangeInput}
+        />
+      </label>
+      <button type="submit">LogIn</button>
+    </form>
   );
 };
 
--- src/components/RegisterForm/RegisterForm.jsx
+++ src/components/RegisterForm/RegisterForm.jsx
@@ -1,8 +1,90 @@
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { toast } from 'react-toastify';
+import { register } from 'redux/auth/authThunk';
+
 const RegisterForm = () => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const dispatch = useDispatch();
+
+  const handleChangeInput = e => {
+    const { name, value } = e.currentTarget;
+    switch (name) {
+      case 'name':
+        setName(value);
+        break;
+      case 'email':
+        setEmail(value);
+        break;
+      case 'password':
+        setPassword(value);
+        break;
+
+      default:
+        break;
+    }
+  };
+  const handleSubmitUser = e => {
+    e.preventDefault();
+    const newUser = { name, email, password };
+    console.log('newUser', newUser);
+    dispatch(register(newUser))
+      .unwrap()
+      .then(originalPromiseResult => {
+        toast.success(`${originalPromiseResult.newUser.name} welcome!`);
+      })
+      .catch(() => {
+        toast.failure("Sorry, something's wrong");
+      });
+    setName('');
+    setEmail('');
+    setPassword('');
+  };
   return (
-    <>
-      <h1>Тут буд форма для регістрації</h1>
-    </>
+    <form onSubmit={handleSubmitUser}>
+      <label>
+        Name
+        <input
+          type="text"
+          name="name"
+          value={name}
+          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+          required
+          placeholder="Enter name ..."
+          onChange={handleChangeInput}
+        />
+      </label>
+      <label>
+        Email
+        <input
+          type="email"
+          name="email"
+          value={email}
+          pattern="/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/."
+          title="Email may contain letters, numbers, an apostrophe, and must be followed by '@' domain name '.' domain suffix. For example Taras@ukr.ua, adrian@gmail.com, JacobM3rcer@hotmail.com"
+          required
+          placeholder="Enter email ..."
+          onChange={handleChangeInput}
+        />
+      </label>
+      <label>
+        Password
+        <input
+          type="password"
+          name="password"
+          value={password}
+          pattern="^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$"
+          title="Password must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters. For example TgeV23592, 3Greioct."
+          required
+          placeholder="Enter password ..."
+          onChange={handleChangeInput}
+        />
+      </label>
+      <button type="submit">Register</button>
+    </form>
   );
 };
 
